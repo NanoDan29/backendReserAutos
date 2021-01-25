87aeaa96eac596eed6690e4f76e0d4144a7e1bac
NanoDan29 <jowell29jm@gmail.com>
2021-01-25 04:51:30 -0500

    Intento 3

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 process.env.PORT = process.env.PORT || 4000
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
 
 
 // ============================
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/reservaAutos';
 } else {
-    urlDB = `mongodb+srv://jhoel:123@cluster0.pudma.mongodb.net/<dbname>?retryWrites=true&w=majority;`
+    urlDB = `mongodb+srv://jhoel:123@cluster0.pudma.mongodb.net/%3Cdbname%3E?retryWrites=true&w=majority;`
 }
 
 process.env.URLDB = urlDB;
