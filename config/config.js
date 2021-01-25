e88f1bff03fb06761c22037bc5c65302c5d870b2
NanoDan29 <jowell29jm@gmail.com>
2021-01-25 05:29:33 -0500

    intento 8

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/reservaAutos";
 } else {
-    urlDB = "mongodb+srv://autos:123@cluster0.kgukc.mongodb.net/autos?retryWrites=true&w=majority"
+    urlDB = "mongodb+srv://autos:autos@cluster0.kgukc.mongodb.net/autos?retryWrites=true&w=majority"
 }
 
 process.env.URLDB = urlDB;
